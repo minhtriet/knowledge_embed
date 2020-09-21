cebb8be4f2b80554c99f2690757229d5fe03eabb
minhtriet <minhtriet09@gmail.com>
2020-09-21 14:12:15 +0200

    nn definition

--- nn.py
+++ nn.py
@@ -23,63 +23,76 @@ class DistMultNN(nn.Module):
         self.rnn = torch.nn.RNN(input_size=encoding_dim, hidden_size=1, num_layers=1, nonlinearity='relu')
         self.loss_func = torch.nn.LogSoftmax(dim=1)
 
-    def forward(self, sample):
+    def loss(self, y_pred):
+        """
+        compute the batch loss
+        """
+        softmax = -1 * self.loss_func(y_pred)
+        result = torch.mean(softmax[:, 0])
+        return result
+
+    def forward(self, samples):
         """
         Deal with a single train item -> batch
-        samples ((e1,r,e2), label)
+        samples [(e1,r,e2), ...]
         """
-        entity_1 = self.entities_embedding[sample[0]]
-        entity_2 = self.entities_embedding[sample[2]]
-        r = self.relation_embedding[sample[1]]
-
-        y_1 = torch.nn.ReLU(self.w.mm(entity_1))
-        y_2 = torch.nn.ReLU(self.w.mm(entity_2))
-        r_out = y_1.t().mm(r).mm(y_2)
-
-        loss_fn = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(self.parameters(), lr=0.001)
-        model = DistMultNN().to(rank)
-        ddp_model = DDP(self, device_ids=[rank])
-        optimizer.zero_grad()
-        outputs = ddp_model(torch.randn(20, 10))
-        labels = torch.randn(20, 5).to(rank)
-        loss_fn(outputs, labels).backward()
-        optimizer.step()
-        loss.backward()
+        batch_size = samples.shape[0]
+        e1s = torch.gather(samples, 1, torch.tensor([[0]*batch_size]).view(-1,1))
+        rs = torch.gather(samples, 1, torch.tensor([[1]*batch_size]).view(-1,1))
+        e2s = torch.gather(samples, 1, torch.tensor([[2]*batch_size]).view(-1,1))
+        entity_1 = self.entities_embedding(e1s)
+        entity_2 = self.entities_embedding(e2s)
+        r = self.relation_embedding(rs)
+        x = self.harmonic_holograhpic_embedding(entity_1, r, entity_2)
+        batch_result = self.score(x)
+        return batch_result
 
     def _circular_correl(self, e1, e2):
-        return torch.irfft(torch.conj(torch.rfft(e1, 1)) * torch.rfft(e2, 1), 1).real
+        ccorel = torch.irfft(torch.conj(torch.rfft(e1, 1, onesided=False)) * torch.rfft(e2, 1, onesided=False),
+                           1, onesided=False)
+        return ccorel.transpose(1,2)
 
-    def holograhpic_embedding(self, e1, r, e2):
+    def harmonic_holograhpic_embedding(self, e1, r, e2):
         """
         from one hot encoding of e1, r, e2 to
-        holograhpic_embedding. Eq. 9
+        harmonic holograhpic_embedding. Eq. 11
+        * is element wise multiplication
         """
-        return r.t().mm(self._circular_correl(e1, e2))
+        return r * self._circular_correl(e1, e2)
 
     def H(self, h, x):
         """
         H is the harmony score of h and an relation
         embedding x. Equation (1) in paper
 
-        `x` is (e_l, r, e_r)
-        `h` is a reference point somehow??
+        `x` is the embedding of (e_l, r, e_r)
+        `h` is a column vector (x, 1)
         """
+        # h = next(self.rnn.parameters())  # hidden weight, row vector
         diff = h - x
-        return h.t().mm(self.W).mm(h) + self.b.t().mm(h) - self.lambda_.mm(diff.t().mm(diff))
+        result = h.t(1,2).mm(self.W).mm(h) + h.mm(self.b) \
+                 - self.lambda_ * diff.norm() ** 2  # this line is a hack for the dot product
+        assert result.shape == torch.Size([1, 1])
+        result = result.item()
+        return result
 
     def muy(self, x):
         """
         eq. 3
         x is an embedding of a relation
         """
-        V = self.W - self.lambda_ * torch.eye(self.ENCODING_DIM)
-        # todo recalculate V after every step
-        return -0.5 * V.inverse() * self.b + self.lambda_ * x
+        V = self.W - (self.lambda_ * torch.eye(self.ENCODING_DIM))
+        V = V.inverse()
+        r = torch.einsum('...ij,jk', V, -0.5*self.b + self.lambda_ * x)
+        assert r.shape == (V.shape[0], V.shape[1])
+        for i in range(V.shape[0]):
+            assert V[i].mm(-0.5*self.b + self.lambda_ * x).allclose(r[i])
+        return r
 
-    def loss(self, e1, r, e2, embed_func):
+
+    def score(self, x):
         """
+        embed_func is harmonic_holograhpic_embedding
         Eq. 13
         """
-        x = embed_func(e1, r, e2)
-        self.H(self.muy(x), embed_func(e1, r, e2))
+        return self.H(self.muy(x), x)
