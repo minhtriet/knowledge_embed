e2bda471263e98ebab5e3fb115fbe392e8141f2b
minhtriet <minhtriet09@gmail.com>
2020-09-23 16:20:52 +0200

    remove unnecessary files

--- test_nn.py
+++
@@ -1,8 +0,0 @@
-from nn import DistMultNN
-import torch
-
-def test_event_embedding():
-    x = DistMultNN("fb15k")
-    x.holograhpic_embedding(torch.tensor([1,0,0,0], dtype=torch.float).reshape((4,1)),
-                            torch.tensor([0,1,0], dtype=torch.float).reshape((3,1)),
-                            torch.tensor([0,1,0,0], dtype=torch.float).reshape((4,1)))
