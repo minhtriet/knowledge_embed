03a965b1940f37497702eb99873c3ab96772ced2
minhtriet <minhtriet09@gmail.com>
2020-09-23 16:23:09 +0200

    fix test

--- test_dataset.py
+++ test_dataset.py
@@ -1,17 +1,16 @@
 import data
-from torch import Size
 import pytest
+
 no_negsamples = 10
 
-
 testdata = [
-    ("train", 0, (0,0,1)), ("train", -1, (8867, 565, 4237)), ("train", 49, (96, 41, 24)),
-#    ("val", 0, (5167, 52, 1427)), ("val", -1, (1142, 166, 2156)), ("val", 49, (9071, 43, 101)),
-#    ("test", 0, (453, 37, 1347)), ("test", -1, (1814, 11, 480)), ("test", 49, (764, 46, 779)),
+    ("train", 0, (0, 0, 1)), ("train", -1, (8867, 565, 4237)), ("train", 49, (96, 41, 24)),
+    ("dev", 0, (5167, 52, 1427)), ("dev", -1, (1142, 166, 2156)), ("dev", 49, (9071, 43, 101)),
+    ("test", 0, (453, 37, 1347)), ("test", -1, (764, 46, 779)), ("test", 49, (1814, 11, 480)),
 ]
 
 
 @pytest.mark.parametrize("slice, index, expect", testdata)
-def testTrainData(slice, index, expect):
-    ds = data.RelationDataset("data/FB15K", no_negsamples=no_negsamples)
-    assert (ds.train_data[0][index], ds.train_data[1][index], ds.train_data[2][index]) == expect
+def test_train_data(slice, index, expect):
+    ds = data.RelationDataset("data/FB15K", no_negsamples=no_negsamples, slice=slice)
+    assert (ds.data[0][index], ds.data[1][index], ds.data[2][index]) == expect
